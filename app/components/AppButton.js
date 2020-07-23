8828ed6918c6de261a9b0049e4e6733ea2fc66e5
Meghanath91 <meghanath.balaji@gmail.com>
2020-07-23 00:31:23 -0400

    setup project and install dependencies

---
+++ app/components/AppButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+import colors from "../config/colors";
+
+const AppButton = ({ title, onPress, color = colors.primary }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: colors[color] }]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    height: 50,
+    width: "100%",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  text: {
+    letterSpacing: 5,
+    color: colors.white,
+    fontWeight: "600",
+    letterSpacing: 5,
+    fontSize: 20,
+  },
+});
+export default AppButton;
