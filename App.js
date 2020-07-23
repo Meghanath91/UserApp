77d33c701dc32c7043132ae6c7bbada514ce64bf
Meghanath91 <meghanath.balaji@gmail.com>
2020-07-23 01:35:55 -0400

    design welcome screen- fix color and logo

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,54 +1,53 @@
-// import React from "react";
-// import { ImageBackground, StyleSheet, View, Text, Image } from "react-native";
-// import AppButton from "../components/AppButton";
-// import colors from "../config/colors";
+import React from "react";
+import { ImageBackground, StyleSheet, View, Text, Image } from "react-native";
+import AppButton from "../components/AppButton";
 
-// function WelcomeScreen({ navigation }) {
-//   return (
-//     <ImageBackground
-//       blurRadius={2}
-//       style={styles.background}
-//       source={require("../assets/background.jpg")}
-//     >
-//       <View style={styles.logoContainer}>
-//         <Image source={require("../assets/logo.png")} style={styles.logo} />
-//         <Text style={styles.tagline}>Join Us Today</Text>
-//       </View>
-//       <View style={styles.buttonContainer}>
-//         <AppButton title="LOGIN" onPress={() => console.log("login")} />
-//         <AppButton
-//           title="REGISTER"
-//           color={colors.secondary}
-//           onPress={() => console.log("Register")}
-//         />
-//       </View>
-//     </ImageBackground>
-//   );
-// }
-// const styles = StyleSheet.create({
-//   background: {
-//     flex: 1,
-//     justifyContent: "flex-end",
-//     alignItems: "center",
-//   },
+function WelcomeScreen({ navigation }) {
+  return (
+    <ImageBackground
+      blurRadius={2}
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo.png")} style={styles.logo} />
+        <Text style={styles.tagline}>Join Us Today</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <AppButton title="LOGIN" onPress={() => console.log("login")} />
+        <AppButton
+          title="REGISTER"
+          color="secondary"
+          onPress={() => console.log("Register")}
+        />
+      </View>
+    </ImageBackground>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
 
-//   logo: {
-//     width: 80,
-//     height: 80,
-//   },
-//   logoContainer: {
-//     position: "absolute",
-//     top: 70,
-//     alignItems: "center",
-//   },
-//   buttonContainer: {
-//     padding: 20,
-//     width: "100%",
-//   },
-//   tagline: {
-//     fontSize: 25,
-//     paddingVertical: 20,
-//     fontWeight: "700",
-//   },
-// });
-// export default WelcomeScreen;
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    padding: 20,
+    width: "100%",
+  },
+  tagline: {
+    fontSize: 25,
+    paddingVertical: 20,
+    fontWeight: "700",
+  },
+});
+export default WelcomeScreen;
